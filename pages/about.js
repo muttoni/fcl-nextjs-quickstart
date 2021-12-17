f96ae17f9cb3774b0543fcb851683f9921f8a3b6
Andrea Muttoni <maverickmuttoni+github@gmail.com>
2021-12-17 16:32:37 +0100

    updated

--- pages/about.js
+++ pages/about.js
@@ -8,16 +8,14 @@ export default function About() {
       </Head>
       <div className="container">
         <h1>About this app</h1>
-
         <p>
           This is a <a href="https://docs.onflow.org/fcl">FCL-powered</a> app built on <a href="http://onflow.org">Flow</a>.
         </p>
-
         <p>
           This app shows how to:
         </p>
         <ul>
-          <li>Authenticate a user on the Flow blockchain.</li> 
+          <li>Authenticate a user on the Flow blockchain.</li>
           <li>Import a contract to let a user create a profile and query the blockchain for their data.</li>
           <li>Let a user modify their profile and mutate the blockchain to save changes.</li>
         </ul>
