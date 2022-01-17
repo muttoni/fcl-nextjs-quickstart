3c57d020ca65d9b6b93f2593e0a4a8793e4a7928
Michael Ski <fubz.dev@gmail.com>
2022-01-16 20:13:30 -0500

    fix: use environment variables to configure local development vs testnet deployment

--- flow/config.js
+++ flow/config.js
@@ -1,12 +1,8 @@
 import { config } from "@onflow/fcl";
 
 config({
-  "accessNode.api": "http://localhost:8080",
-  // "accessNode.api": "https://access-testnet.onflow.org",
-  "discovery.wallet": "http://localhost:8701/fcl/authn",
-  // "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",
-  "0xProfile": "0xf8d6e0586b0a20c7", // The account address where the smart contract lives
-  // "0xProfile": "0xba1132bc08f82fe2" // The account address where the smart contract lives
-  "app.detail.icon": "http://localhost:3000/flow-logo.svg",
-  "app.detail.title": "Flow Next.js Quick Start"
+  "app.detail.title": "Flow Next.js Quick Start",
+  "accessNode.api": process.env.NEXT_PUBLIC_ACCESS_NODE_API,
+  "discovery.wallet": process.env.NEXT_PUBLIC_DISCOVERY_WALLET,
+  "0xProfile": process.env.NEXT_PUBLIC_CONTRACT_PROFILE, // The account address where the smart contract lives
 })
